13593def346b4d1cdd6d9388a1485fdd6ec25b1f
Paul <kruesselpaul@gmail.com>
2025-05-20 13:55:55 +0200

    Created IntelliJ Jakarta Project

---
+++ foodies/src/main/java/com/foodies/foodies/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.foodies.foodies;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
